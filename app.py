c9e33d2c3e3d42d55bebdbe7a0080b5b31f25161
willd35 <wdaly@terpmail.umd.edu>
2024-03-25 22:27:12 -0400

    created routes for contact and favorite-course html with flask functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,9 @@ def hello_world():  # put application's code here
 
 @app.route('/hello')
 def hello():  # put application's code here
+    print('First name entered: ' + request.args.get('first_name'))
+    print('Last name entered: ' + request.args.get('last_name'))
+
     return render_template('hello.html')
 
 @app.route('/about')
@@ -18,6 +21,19 @@ def about():  # put application's code here
 @app.route('/about-css')
 def about_css():  # put application's code here
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():  # put application's code here
+    print('Course subject entered: ' + request.args.get('course_subject'))
+    print('Course number entered: ' + request.args.get('course_number'))
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():  # put application's code here
+    if request.method =='POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
