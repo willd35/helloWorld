413dfc596422723cc70dd576126ceb47682453bd
willd35 <wdaly@terpmail.umd.edu>
2024-04-02 12:07:22 -0400

    edited student_create route to include email request to prevent an error from happening when adding a student

--- app.py
+++ app.py
@@ -46,12 +46,13 @@ def student_create():
     elif request.method == 'POST':
         first_name = request.form['first_name']
         last_name = request.form['last_name']
+        email = request.form['email']
         major_id = request.form['major_id']
 
         birth_date = request.form['birth_date']
         is_honors = True if 'is_honors' in request.form else False
 
-        student = Student(first_name=first_name, last_name=last_name, major_id=major_id,
+        student = Student(first_name=first_name, last_name=last_name, email=email , major_id=major_id,
                           birth_date=dt.strptime(birth_date, '%Y-%m-%d'), is_honors=is_honors)
         db.session.add(student)
         db.session.commit()
